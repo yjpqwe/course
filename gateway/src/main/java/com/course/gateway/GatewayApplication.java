8f64c2004dfefacc4f83977d4a2b4a9907acfd89
qq_connect-123QWE <592926935@qq.com>
2020-10-22 22:27:18 +0800

    1.新增gateway子模块，增加gateway依赖，并注册到注册中心

---
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.course.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+	/**
+	 * 日志框架固定的一个写法
+	 * @param args
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+
+}
