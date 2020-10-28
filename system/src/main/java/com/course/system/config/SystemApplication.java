7a58a828c59ed6fa186cb012e4c7217b965ca8b1
qq_connect-123QWE <592926935@qq.com>
2020-10-28 22:57:27 +0800

    1.将通用的配置移动到server 注：server配置文件的路径要放在resources/config/application.properties，不能和上层的路径一样放在resource根目录下

--- system/src/main/java/com/course/system/config/SystemApplication.java
+++ system/src/main/java/com/course/system/config/SystemApplication.java
@@ -1,14 +1,18 @@
-package com.course.system;
+package com.course.system.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.server.mapper")
 public class SystemApplication {
 	/**
 	 * 日志框架固定的一个写法
--- system/src/main/java/com/course/system/domain/Test.java
+++
@@ -1,4 +0,0 @@
-package com.course.system.domain;
-
-public class Test {
-}
